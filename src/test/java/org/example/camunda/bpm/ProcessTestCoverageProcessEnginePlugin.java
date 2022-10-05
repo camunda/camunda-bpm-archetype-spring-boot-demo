68f3b4064a01017dc9aee64e1ea08970efad8fce
ci.cambpm <ci_automation@camunda.com>
2022-10-05 14:20:01 +0200

    Upgrade to 7.17.0

--- src/test/java/org/example/camunda/bpm/ProcessTestCoverageProcessEnginePlugin.java
+++ src/test/java/org/example/camunda/bpm/ProcessTestCoverageProcessEnginePlugin.java
@@ -3,7 +3,7 @@ package org.example.camunda.bpm;
 import org.camunda.bpm.engine.impl.cfg.AbstractProcessEnginePlugin;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
-import org.camunda.bpm.extension.process_test_coverage.junit.rules.ProcessCoverageConfigurator;
+import org.camunda.bpm.extension.process_test_coverage.engine.ProcessCoverageConfigurator;
 import org.springframework.stereotype.Component;
 
 @Component
